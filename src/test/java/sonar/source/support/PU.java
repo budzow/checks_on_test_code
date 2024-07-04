636ecce6d9b3519746c694281630dadc71e302f8
Michal Budzowski <michal.budzowski@sonarsource.com>
2024-07-04 16:44:17 +0200

    add files

---
+++ src/test/java/sonar/source/support/PU.java
@@ -0,0 +1,21 @@
+package sonar.source.support;
+
+import java.io.*;
+
+
+public class PU 
+{
+    
+    public void func1()
+    {
+    }
+	
+	
+    public void func2()
+    {
+    }	
+	
+	private void unusedPrivate()
+	{
+	}
+}
